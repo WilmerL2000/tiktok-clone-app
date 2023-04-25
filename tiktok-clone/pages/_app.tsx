d8458cbd500262f0c65a599bc9700b70edcad885
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-04-25 17:04:51 -0600

    Changing config

--- tiktok-clone/pages/_app.tsx
+++ tiktok-clone/pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Layout } from '@/components';
+import '@/styles/globals.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
